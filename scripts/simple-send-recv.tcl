6ae1ba429d5ab1013f09e2c19b0ef6b641132e53
Inho Cho <inhojoe@gmail.com>
2017-10-12 14:11:47 +0900

    Simple send & receive script added.

---
+++ scripts/simple-send-recv.tcl
@@ -0,0 +1,29 @@
+set ns [new Simulator]
+set nt [open trace.tr w]
+set agent0 [new Agent/XPass]
+set agent1 [new Agent/XPass]
+
+Node set multiPath_ 1
+
+set node0 [$ns node]
+set node1 [$ns node]
+
+proc finish {} {
+  global ns nt
+  $ns flush-trace
+  close $nt
+  exit 0
+}
+
+$ns trace-all $nt
+
+$ns duplex-link $node0 $node1 10Gb 10us XPassDropTail
+
+$ns attach-agent $node0 $agent0
+$ns attach-agent $node1 $agent1
+
+$ns connect $agent0 $agent1
+
+$ns at 0.0 "$agent0 advance-bytes 3000"
+$ns at 1.0 "finish"
+$ns run
