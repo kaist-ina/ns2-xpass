d3603bdd798ca37b4266d1270429230d3bcba8b8
Inho Cho <inhojoe@gmail.com>
2017-10-26 17:52:31 +0900

    Large-scale fatree script

---
+++ scripts/large-scale-fattree.tcl
@@ -0,0 +1,268 @@
+set ns [new Simulator]
+#
+# Flow configurations
+#
+set numFlow 100000
+set workload "cachefollower" ;# cachefollower, mining, search, webserver
+set linkLoad 0.6 ;# ranges from 0.0 to 1.0
+
+#
+# Toplogy configurations
+#
+set linkRate 10 ;# Gb
+set hostDelay 0.000001 ;# secs
+set linkDelayHostTor 0.000004 ;# secs
+set linkDelayTorAggr 0.000004 ;# secs
+set linkDelayAggrCore 0.000004 ;# secs
+set dataBufferHost [expr 1000*1538] ;# bytes / port
+set dataBufferFromTorToAggr [expr 250*1538] ;# bytes / port
+set dataBufferFromAggrToCore [expr 250*1538] ;# bytes / port
+set dataBufferFromCoreToAggr [expr 250*1538] ;# bytes / port
+set dataBufferFromAggrToTor [expr 250*1538] ;# bytes / port
+set dataBufferFromTorToHost [expr 250*1538] ;# bytes / port
+
+set numCore 8 ;# number of core switches
+set numAggr 16 ;# number of aggregator switches
+set numTor 32 ;# number of ToR switches
+set numNode 192 ;# number of nodes
+
+#
+# XPass configurations
+#
+set alpha 0.0625
+set w_init 0.0625
+set creditBuffer [expr 84*8]
+set maxCreditBurst [expr 84*2]
+set minJitter -0.1
+set maxJitter 0.1
+set minEthernetSize 84
+set maxEthernetSize 1538
+set minCreditSize 76
+set maxCreditSize 92
+set xpassHdrSize 78
+set maxPayload [expr $maxEthernetSize-$xpassHdrSize]
+set avgCreditSize [expr ($minCreditSize+$maxCreditSize)/2.0]
+set creditBW [expr $linkRate*125000000*$avgCreditSize/($avgCreditSize+$maxEthernetSize)]
+set creditBW [expr int($creditBW)]
+
+#
+# Simulation setup
+#
+set simStartTime 0.1
+set simEndTime 60
+
+# Output file
+file mkdir "outputs"
+set nt [open outputs/trace.out w]
+set fct_out [open outputs/fct.out w]
+puts $fct_out "Flow ID,Flow Size (bytes),Flow Completion Time (secs)"
+close $fct_out
+
+set flowfile [open flowfile.tr w]
+
+proc finish {} {
+  global ns nt flowfile
+  $ns flush-trace
+  close $nt
+  close $flowfile
+  puts "Simulation terminated successfully."
+  exit 0
+}
+#$ns trace-all $nt
+
+# Basic parameter settings
+Agent/XPass set min_credit_size_ $minCreditSize
+Agent/XPass set max_credit_size_ $maxCreditSize
+Agent/XPass set min_ethernet_size_ $minEthernetSize
+Agent/XPass set max_ethernet_size_ $maxEthernetSize
+Agent/XPass set max_credit_rate_ $creditBW
+Agent/XPass set alpha_ $alpha
+Agent/XPass set target_loss_scaling_ 0.125
+Agent/XPass set w_init_ $w_init
+Agent/XPass set min_w_ 0.01
+Agent/XPass set retransmit_timeout_ 0.0001
+Agent/XPass set min_jitter_ $minJitter
+Agent/XPass set max_jitter_ $maxJitter
+
+Queue/XPassDropTail set credit_limit_ $creditBuffer
+Queue/XPassDropTail set max_tokens_ $maxCreditBurst
+Queue/XPassDropTail set token_refresh_rate_ $creditBW
+
+DelayLink set avoidReordering_ true
+$ns rtproto DV
+Agent/rtProto/DV set advertInterval 10
+Node set multiPath_ 1
+Classifier/MultiPath set symmetric_ true
+
+# Workloads setting
+if {[string compare $workload "mining"] == 0} {
+  set workloadPath "workloads/workload_mining.tcl"
+  set avgFlowSize 7410212
+} elseif {[string compare $workload "search"] == 0} {
+  set workloadPath "workloads/workload_search.tcl"
+  set avgFlowSize 1654275
+} elseif {[string compare $workload "cachefollower"] == 0} {
+  set workloadPath "workloads/workload_cachefollower.tcl"
+  set avgFlowSize 701490
+} elseif {[string compare $workload "webserver"] == 0} {
+  set workloadPath "workloads/workload_webserver.tcl"
+  set avgFlowSize 63735
+} else {
+  puts "Invalid workload: $workload"
+  exit 0
+}
+
+set overSubscRatio [expr double($numNode/$numTor)/double($numTor/$numAggr)*1.5]
+set lambda [expr ($numNode*$linkRate*1000000000*$linkLoad)/($avgFlowSize*8.0/$maxPayload*$maxEthernetSize)]
+set avgFlowInterval [expr $overSubscRatio/$lambda]
+
+# Random number generators
+set RNGFlowSize [new RNG]
+$RNGFlowSize seed 61569011
+
+set RNGFlowInterval [new RNG]
+$RNGFlowInterval seed 94762103
+
+set RNGSrcNodeId [new RNG]
+$RNGSrcNodeId seed 17391005
+
+set RNGDstNodeId [new RNG]
+$RNGDstNodeId seed 35010256
+
+set randomFlowSize [new RandomVariable/Empirical]
+$randomFlowSize use-rng $RNGFlowSize
+$randomFlowSize set interpolation_ 2
+$randomFlowSize loadCDF $workloadPath
+
+set randomFlowInterval [new RandomVariable/Exponential]
+$randomFlowInterval use-rng $RNGFlowInterval
+$randomFlowInterval set avg_ $avgFlowInterval
+
+set randomSrcNodeId [new RandomVariable/Uniform]
+$randomSrcNodeId use-rng $RNGSrcNodeId
+$randomSrcNodeId set min_ 0
+$randomSrcNodeId set max_ $numNode
+
+set randomDstNodeId [new RandomVariable/Uniform]
+$randomDstNodeId use-rng $RNGDstNodeId
+$randomDstNodeId set min_ 0
+$randomDstNodeId set max_ $numNode
+
+# Node
+puts "Creating nodes..."
+for {set i 0} {$i < $numNode} {incr i} {
+  set dcNode($i) [$ns node]
+}
+for {set i 0} {$i < $numTor} {incr i} {
+  set dcTor($i) [$ns node]
+}
+for {set i 0} {$i < $numAggr} {incr i} {
+  set dcAggr($i) [$ns node]
+}
+for {set i 0} {$i < $numCore} {incr i} {
+  set dcCore($i) [$ns node]
+}
+
+# Link
+puts "Creating links..."
+for {set i 0} {$i < $numAggr} {incr i} {
+  set coreIndex [expr $i%2]
+  for {set j $coreIndex} {$j < $numCore} {incr j 2} {
+    $ns simplex-link $dcAggr($i) $dcCore($j) [set linkRate]Gb $linkDelayAggrCore XPassDropTail
+    set link_aggr_core [$ns link $dcAggr($i) $dcCore($j)]
+    set queue_aggr_core [$link_aggr_core queue]
+    $queue_aggr_core set data_limit_ $dataBufferFromAggrToCore
+
+    $ns simplex-link $dcCore($j) $dcAggr($i) [set linkRate]Gb $linkDelayAggrCore XPassDropTail
+    set link_core_aggr [$ns link $dcCore($j) $dcAggr($i)]
+    set queue_core_aggr [$link_core_aggr queue]
+    $queue_core_aggr set data_limit_ $dataBufferFromCoreToAggr
+  }
+}
+
+for {set i 0} {$i < $numTor} {incr i} {
+  set aggrIndex [expr $i/4*2]
+  for {set j $aggrIndex} {$j <= $aggrIndex+1} {incr j} {
+    $ns simplex-link $dcTor($i) $dcAggr($j) [set linkRate]Gb $linkDelayTorAggr XPassDropTail
+    set link_tor_aggr [$ns link $dcTor($i) $dcAggr($j)]
+    set queue_tor_aggr [$link_tor_aggr queue]
+    $queue_tor_aggr set data_limit_ $dataBufferFromTorToAggr
+
+    $ns simplex-link $dcAggr($j) $dcTor($i) [set linkRate]Gb $linkDelayTorAggr XPassDropTail
+    set link_aggr_tor [$ns link $dcAggr($j) $dcTor($i)]
+    set queue_aggr_tor [$link_aggr_tor queue]
+    $queue_aggr_tor set data_limit_ $dataBufferFromAggrToTor
+  }
+}
+
+for {set i 0} {$i < $numNode} {incr i} {
+  set torIndex [expr $i/($numNode/$numTor)]
+
+  $ns simplex-link $dcNode($i) $dcTor($torIndex) [set linkRate]Gb [expr $linkDelayHostTor+$hostDelay] XPassDropTail
+  set link_host_tor [$ns link $dcNode($i) $dcTor($torIndex)]
+  set queue_host_tor [$link_host_tor queue]
+  $queue_host_tor set data_limit_ $dataBufferHost
+
+  $ns simplex-link $dcTor($torIndex) $dcNode($i) [set linkRate]Gb $linkDelayHostTor XPassDropTail
+  set link_tor_host [$ns link $dcTor($torIndex) $dcNode($i)]
+  set queue_tor_host [$link_tor_host queue]
+  $queue_tor_host set data_limit_ $dataBufferFromTorToHost
+}
+
+puts "Creating agents and flows..."
+for {set i 0} {$i < $numFlow} {incr i} {
+  set src_nodeid [expr int([$randomSrcNodeId value])]
+  set dst_nodeid [expr int([$randomDstNodeId value])]
+
+  while {$src_nodeid == $dst_nodeid} {
+#    set src_nodeid [expr int([$randomSrcNodeId value])]
+    set dst_nodeid [expr int([$randomDstNodeId value])]
+  }
+
+  set sender($i) [new Agent/XPass]
+  set receiver($i) [new Agent/XPass]
+
+  $sender($i) set fid_ $i
+  $sender($i) set host_id_ $src_nodeid
+  $receiver($i) set fid_ $i
+  $receiver($i) set host_id_ $dst_nodeid
+
+  $ns attach-agent $dcNode($src_nodeid) $sender($i)
+  $ns attach-agent $dcNode($dst_nodeid) $receiver($i)
+
+  $ns connect $sender($i) $receiver($i)
+
+  $ns at $simEndTime "$sender($i) close"
+  $ns at $simEndTime "$receiver($i) close"
+
+  set srcIndex($i) $src_nodeid
+  set dstIndex($i) $dst_nodeid
+}
+
+set nextTime $simStartTime
+set fidx 0
+
+proc sendBytes {} {
+  global ns random_flow_size nextTime sender fidx randomFlowSize randomFlowInterval numFlow srcIndex dstIndex flowfile
+  while {1} {
+    set fsize [expr ceil([expr [$randomFlowSize value]])]
+    if {$fsize > 0} {
+      break;
+    }
+  }
+
+  puts $flowfile "$nextTime $srcIndex($fidx) $dstIndex($fidx) $fsize"
+  $ns at $nextTime "$sender($fidx) advance-bytes $fsize"
+
+  set nextTime [expr $nextTime+[$randomFlowInterval value]]
+  set fidx [expr $fidx+1]
+
+  if {$fidx < $numFlow} {
+    $ns at $nextTime "sendBytes"
+  }
+}
+
+$ns at 0.0 "puts \"Simulation starts!\""
+$ns at $nextTime "sendBytes"
+$ns at [expr $simEndTime+1] "finish"
+$ns run
